4fc5b0db98d2cff35eefe708812e1a08c2830f8f
Ben Leibowitz <benleibowitz@users.noreply.github.com>
2022-08-24 19:05:10 -0400

    Create grubhub.js

---
+++ grubhub.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Grubhub Ad-Remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove sponsored results and ads on Grubhub
+// @author       BL
+// @match        https://www.grubhub.com/search*
+// @match        https://grubhub.com/search*
+// @icon         https://static.wixstatic.com/media/6ff25e_7a99b9ba661e4a7ba1f4de639ce5689e~mv2.jpg/v1/fill/w_52,h_52,al_c,q_80,usm_0.66_1.00_0.01/grubhub-logo.jpg
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        GM_addStyle
+// @grant        GM.getValue
+// ==/UserScript==
+/* global $, waitForKeyElements */
+
+waitForKeyElements('.searchResults-wrapper', runner);
+
+function runner(node) {
+    'use strict';
+
+    var spans = document.querySelectorAll('[data-testid="sponsored-result"]');
+    for (var i = 0; i < spans.length; i++) {
+        var span = spans[i];
+        console.log('Removing sponsored result: ' + span.querySelector('.restaurant-name').title);
+        span.remove();
+    }
+};
