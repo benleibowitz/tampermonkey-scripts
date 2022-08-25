db5f1b37f0605b731415c398a5b0d129d653aa2d
Ben Leibowitz <benleibowitz2013@gmail.com>
2022-08-24 21:35:32 -0400

    Remove ads on startup

--- grubhub.js
+++ grubhub.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Grubhub Ad-Remover
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Remove sponsored results and ads on Grubhub
 // @author       BL
-// @match        https://www.grubhub.com/search*
-// @match        https://grubhub.com/search*
+// @match        https://www.grubhub.com/*
+// @match        https://grubhub.com/*
 // @icon         https://static.wixstatic.com/media/6ff25e_7a99b9ba661e4a7ba1f4de639ce5689e~mv2.jpg/v1/fill/w_52,h_52,al_c,q_80,usm_0.66_1.00_0.01/grubhub-logo.jpg
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -26,6 +26,7 @@ function removeSponsored(ignored) {
 };
 
 waitForKeyElements('.searchResults-wrapper', function(node) {
+    removeSponsored(node);
     var observer = new MutationObserver(removeSponsored);
     observer.observe(document.getElementsByClassName('searchResults-wrapper')[0], {attributes: true, childList: true, characterData: false, subtree:true});
 });
