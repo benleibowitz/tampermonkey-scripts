82e899f948689c51f54086e7071932efbc583b26
benleibowitz <benleibowitz2013@gmail.com>
2024-12-15 12:41:35 -0500

    Add Medium script

---
+++ remove_medium_sign_up_banner.js
@@ -0,0 +1,109 @@
+// ==UserScript==
+// @name         Remove Medium Sign-Up Banner
+// @version      0.0.1
+// @description  Remove Medium sign-up banner
+// @license      MIT
+// @match        https://*.medium.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=medium.com
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    Usage example:
+
+        waitForKeyElements (
+            "div.comments"
+            , commentCallbackFunction
+        );
+
+        //--- Page-specific function to do what we want when the node is found.
+        function commentCallbackFunction (jNode) {
+            jNode.text ("This comment changed by waitForKeyElements().");
+        }
+
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+
+// Pulled from: https://gist.github.com/raw/2625891/waitForKeyElements.js
+function waitForKeyElements(
+  selectorTxt /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */,
+  actionFunction /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */,
+  bWaitOnce /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */,
+  iframeSelector /* Optional: If set, identifies the iframe to
+                        search.
+                    */,
+) {
+  var targetNodes, btargetsFound;
+
+  if (typeof iframeSelector == "undefined") targetNodes = $(selectorTxt);
+  else targetNodes = $(iframeSelector).contents().find(selectorTxt);
+
+  if (targetNodes && targetNodes.length > 0) {
+    btargetsFound = true;
+    /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+    targetNodes.each(function () {
+      var jThis = $(this);
+      var alreadyFound = jThis.data("alreadyFound") || false;
+
+      if (!alreadyFound) {
+        //--- Call the payload function.
+        var cancelFound = actionFunction(jThis);
+        if (cancelFound) btargetsFound = false;
+        else jThis.data("alreadyFound", true);
+      }
+    });
+  } else {
+    btargetsFound = false;
+  }
+
+  //--- Get the timer-control variable for this selector.
+  var controlObj = waitForKeyElements.controlObj || {};
+  var controlKey = selectorTxt.replace(/[^\w]/g, "_");
+  var timeControl = controlObj[controlKey];
+
+  //--- Now set or clear the timer as appropriate.
+  if (btargetsFound && bWaitOnce && timeControl) {
+    //--- The only condition where we need to clear the timer.
+    clearInterval(timeControl);
+    delete controlObj[controlKey];
+  } else {
+    //--- Set a timer, if needed.
+    if (!timeControl) {
+      timeControl = setInterval(function () {
+        waitForKeyElements(
+          selectorTxt,
+          actionFunction,
+          bWaitOnce,
+          iframeSelector,
+        );
+      }, 300);
+      controlObj[controlKey] = timeControl;
+    }
+  }
+  waitForKeyElements.controlObj = controlObj;
+}
+
+(function() {
+    'use strict';
+
+    waitForKeyElements('div[class="m uh ui uj"]', function(nodes) {
+        if (nodes[0]) {
+            nodes[0].remove();
+            console.debug("Removed login banner");
+        }
+    });
+})();
